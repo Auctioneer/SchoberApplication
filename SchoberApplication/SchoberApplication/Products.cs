07825c5aa614c0c300c74b2c10d413d7c499b858
Auctioneer <ljgdavie@hotmail.com>
2015-11-09 16:48:54 +0000

    Added Product class. Currently not used by any class but will be soon.

---
+++ SchoberApplication/SchoberApplication/Products.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoberApplication
+{
+    class Products
+    {
+        private int productID;
+        private string activity;
+        private string type;
+
+        public Products()
+        {
+
+        }
+
+        public Products(int productID, string activity, string type)
+        {
+            this.productID = productID;
+            this.activity = activity;
+            this.type = type;
+        }
+
+        public void setProductID(int productID)
+        {
+            this.productID = productID;
+        }
+
+        public void setActivity(string activity)
+        {
+            this.activity = activity;
+        }
+
+        public void setType(string type)
+        {
+            this.type = type;
+        }
+
+        public int getProductID()
+        {
+            return productID;
+        }
+
+        public string getActivity()
+        {
+            return activity;
+        }
+
+        public string getType()
+        {
+            return type;
+        }
+
+
+    }
+}
